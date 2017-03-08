d4fe380150fabc5dc66d917e0eb7be62a2c00cb2
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-08 10:36:50 -0300

    Criado layout e referenciar o componentes na main activity

--- app/src/main/java/br/com/fiap/a2tina_android_sharedpreferences/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_sharedpreferences/MainActivity.java
@@ -2,12 +2,30 @@ package br.com.fiap.a2tina_android_sharedpreferences;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtUsuario;
+    EditText edtSenha;
+    CheckBox chkSalvar;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUsuario = (EditText)findViewById(R.id.edtusuario);
+        edtSenha = (EditText)findViewById(R.id.edtsenha);
+        chkSalvar = (CheckBox)findViewById(R.id.chkSalvar);
+    }
+
+    public void login(View view) {
+        
     }
 }
